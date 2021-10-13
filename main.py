8150c621c0a599454a7a3446b3efbf194ea94165
ylmzaksoy <81808120+ylmzaksoy@users.noreply.github.com>
2021-10-13 15:42:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ if not os.path.isdir("./downloads"):
 
 Bot.start()
 User.start()
-print("\n[INFO] - STARTED VIDEO PLAYER BOT, JOIN @ASMSAFONE !")
+print("\n[INFO] - BAŞLADI VİDEO OYUNCU BOT, KATIL @EzelHome !")
 
 idle()
 Bot.stop()
 User.stop()
-print("\n[INFO] - STOPPED VIDEO PLAYER BOT, JOIN @ASMSAFONE !")
+print("\n[INFO] - DURDURULDU VİDEO OYUNCU BOT, KATIL @EzelHome !")
