41ed37439729dbc40ace1675d7b41f5d3d374bff
ylmzaksoy <81808120+ylmzaksoy@users.noreply.github.com>
2021-10-13 15:40:41 +0300

    Update translations.py

--- translations.py
+++ translations.py
@@ -2,21 +2,20 @@ from config import ASSISTANT_NAME
 from helpers.bot_utils import BOT_NAME, USERNAME
 
 
-START_TEXT = f"👋🏻 **Hello**, \n\nThis is **{BOT_NAME}** \nI Can Stream Lives, Radios, YouTube Videos & Telegram Audio / Video Files On Voice Chat Of Telegram Groups. Let's Enjoy Cinematic View Of Group Video Player With Your Friends 😉! \n\n**Made With ❤️ By @ImSafone!** 👑"
+START_TEXT = f"👋🏻 **Merhaba**, \n\n**{BOT_NAME}** \nTelegram Gruplarının Sesli Sohbetinde Canlıları, Radyoları, YouTube Videolarını ve Telegram Ses / Video Dosyalarını Yayınlayabilirim. Arkadaşlarınızla Grup Video Oynatıcısının Sinematik Görünümünün Tadını Çıkaralım 😉! \n\n**Teşekkürler❤️ By @TheEzelBoss!** 👑"
 HELP_TEXT = f"""
-🛠-- **Setting Up Bot**:--
+🛠-- **Bot Kurulumu**:--
 
-\u2022 Start Voice Chat In Your Group!
-\u2022 Add Me (@{USERNAME}) & My Assistant (@{ASSISTANT_NAME}) To Your Group!
-\u2022 Give Admin To Me (@{USERNAME}) & My Assistant (@{ASSISTANT_NAME}) In Your Group!
+\u2022 Grubunuzda Sesli Sohbet Başlatın!
+\u2022 Beni (@{USERNAME}) Ve Asistanım (@{ASSISTANT_NAME}) Grubunuza Ekleyin !
+\u2022 Bana (@{USERNAME}) Ve Asistanım (@{ASSISTANT_NAME}) Adminlik Ver !
 
 ⚔️-- **Available Commands**:--
 
-\u2022 `/play` - Stream An Audio
-\u2022 `/stream` - Stream An Video
-\u2022 `/pause` - Pause Current Stream
-\u2022 `/resume` - Resume Paused Stream
-\u2022 `/endstream` - End Stream & Left VC
-\u2022 `/restart` - Restart Bot (Sudo Only)
+\u2022 `/play` - Ses Akışı Başlatır
+\u2022 `/stream` - Video Akışı Başlatır
+\u2022 `/pause` - Geçerli Akışı Duraklatır
+\u2022 `/resume` - Duraklatılmış Akışı Sürdürür
+\u2022 `/endstream` - Yayını Bitir
+\u2022 `/restart` - Botu Yeniden Başlat
 """
-ABOUT_TEXT = f"💡-- **Information**:-- \n\nThis bot is created for streaming videos in telegram group video chats using several methods from WebRTC. Powered by pytgcalls the async client API for the Telegram Group Calls and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots. \n\n**This bot licensed under GNU-GPL 3.0 License!**"
